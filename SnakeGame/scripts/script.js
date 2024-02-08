015e35c30806e967a7f97f969ca6bfd430243a9b
julien-vaz <jhmvaz95@gmail.com>
2024-02-08 20:04:19 -0300

    Variáveis globais criadas

---
+++ SnakeGame/scripts/script.js
@@ -0,0 +1,49 @@
+class Segment {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+/*
+comece criando as variáveis globais do jogo.
+
+    (A) canvas : este é o canvas do html
+    (B) context : este é o contexto do canvas
+    (C) box : este é o tamanho da caixa
+    (D) snake : esta é a cobrinha, a personagem principal do game.
+    (E) direction : este é o sentido/direção em que a cobrinha vai andar.
+    (F) food : é a comida que a cobrinha come e cresce.
+
+    A. o canvas é um elemento html já existente no index.html do boilerplaite que vcs recebeream. Lembre-se como "puxamos" do html os elementos via DOM para criá-lo.
+    */
+   const canvas = document.getElementById("snake");
+
+    /*
+    B. Este é um elemento interno ao canvas, portanto, assim que criado seu canvas, vc pode selecionar seu contexto, neste caso ele é 2d (documentação pode te ajudar).
+    */
+   const context = canvas.getContext("2d");
+
+    /*
+    C. Este é o tamanho geral do campo de jogo. Ele é um integer de valor 32.
+    */
+    let box = 32;
+
+    /*
+    D. Esta é a personagem principal, cobrinha. Ela é um array de coordenadas multiplicadas pelo tamanho do box.
+
+        A posição inicial da snake é x: 8 e y:8 multiplicados pelo valor de box.
+    */
+    let snake = [];
+    snake[0] = new Segment(8 * box, 8 * box);
+
+    /*
+    E. Seu valor é uma string e o default é "right".
+    */
+    let direction = "right";
+
+    /*
+    F. É um objeto de coordenadas aleatórias;
+    */
+    let food;
+    debugger
